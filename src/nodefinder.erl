dc5752fad54a7885233d80f96e55311aea4c011c
Boris Murashov <bottleneko@gmail.com>
2019-04-17 22:22:56 +0300

    Ability to specify network interface

--- src/nodefinder.erl
+++ src/nodefinder.erl
@@ -34,7 +34,12 @@ start (_Type, _Args) ->
   { ok, Addr } = application:get_env (nodefinder, addr),
   { ok, Port } = application:get_env (nodefinder, port),
   { ok, Ttl } = application:get_env (nodefinder, multicast_ttl),
-  nodefindersup:start_link (Addr, Port, Ttl).
+  case application:get_env (nodefinder, if_name) of
+    { ok, IfName } ->
+      nodefindersup:start_link (Addr, Port, Ttl, IfName);
+    undefined ->
+      nodefinder:start_link (Addr, Port, Ttl)
+  end.
 
 %% @hidden
 
