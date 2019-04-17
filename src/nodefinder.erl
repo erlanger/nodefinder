03c86b70643a82aeef49e3555738551b99aa5eb2
Boris Murashov <bottleneko@gmail.com>
2019-04-17 20:18:01 +0300

    Backward compatibility

--- src/nodefinder.erl
+++ src/nodefinder.erl
@@ -34,8 +34,12 @@ start (_Type, _Args) ->
   { ok, Addr } = application:get_env (nodefinder, addr),
   { ok, Port } = application:get_env (nodefinder, port),
   { ok, Ttl } = application:get_env (nodefinder, multicast_ttl),
-  { ok, IfName } = application:get_env (nodefinder, if_name),
-  nodefindersup:start_link (Addr, Port, Ttl, IfName).
+  case application:get_env (nodefinder, if_name) of
+    { ok, IfName } ->
+      nodefindersup:start_link (Addr, Port, Ttl, IfName);
+    undefined ->
+      nodefinder:start_link (Addr, Port, Ttl)
+  end.
 
 %% @hidden
 
